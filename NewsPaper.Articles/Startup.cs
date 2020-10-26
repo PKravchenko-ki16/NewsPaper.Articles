bfc7207410e17438a226c552ba5203a7b0073bc2
Павел Кравченко <pavellevap98@mail.ru>
2020-10-27 02:50:18 +0300

    relocate ConfigureServicesMassTransit to NuGet Package

--- NewsPaper.Articles/Startup.cs
+++ NewsPaper.Articles/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using NewsPaper.Articles.ConfigureServices;
+using NewsPaper.MassTransit.Configuration;
 
 namespace NewsPaper.Articles
 {
@@ -18,10 +18,15 @@ namespace NewsPaper.Articles
 
         public void ConfigureServices(IServiceCollection services)
         {
+            var section = Configuration.GetSection("MassTransit");
             services.AddControllers();
-            ConfigureServicesMassTransit.ConfigureServices(services, Configuration);
-
+            ConfigureServicesMassTransit.ConfigureServices(services, Configuration, new MassTransitConfiguration()
+            {
+                IsDebug = section.GetValue<bool>("IsDebug"),
+                ServiceName = "Articles"
+            });
         }
+    
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
